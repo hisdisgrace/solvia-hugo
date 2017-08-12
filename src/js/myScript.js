1fb4b3ddf01fcf5897cbf3a875212a408f13a8d6
Chrisp <ultrachrisp@gmail.com>
2017-08-12 09:55:31 +0200

    Header coming along

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,3 +1,44 @@
-// particlesJS.load('particles-js', 'js/particles.json', function() {
-//   console.log('particles.js loaded - callback');
-// });
+(function( global ) {
+    var Module = (function() {
+                      
+    var data = "secret";
+                      return {
+                          // This is some boolean property
+                          bool: true,
+                          // Some string value
+                          string: "a string",
+                          // An array property
+                          array: [ 1, 2, 3, 4 ],
+                          // An object property
+                          object: {
+                              lang: "en-Us"
+                          },
+                          getData: function() {
+                              // get the current value of `data`
+                              return data;
+                          },
+                          setHeight: function( value ) {
+                              // set the value of `data` and return it
+                              console.log('Got here');
+                              var elem = document.getElementById('headerImage');
+                              return (
+                                   elem.setAttribute( 'style', 'height:'+value+'px' )
+                                     );
+                              
+                          },
+                          
+                          helloWorld: function( value ) {
+                              // set the value of `data` and return it
+                              return ( console.log('Hello World.'+window.innerHeight) );
+                          }
+                      };
+  })();
+    
+    // Other things might happen here
+    
+    // expose our module to the global object
+    global.Module = Module;
+    Module.helloWorld();
+    Module.setHeight(window.innerHeight);
+    
+})( this );
