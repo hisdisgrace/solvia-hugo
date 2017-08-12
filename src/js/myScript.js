316cf74b0c4dd90fd60662dad71aeb3ba3418a15
Chrisp <ultrachrisp@gmail.com>
2017-08-12 17:16:38 +0200

    Getting distance from top

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -1,44 +1,41 @@
 (function( global ) {
-    var Module = (function() {
+    var Module =
+        (function() {
                       
-    var data = "secret";
-                      return {
-                          // This is some boolean property
-                          bool: true,
-                          // Some string value
-                          string: "a string",
-                          // An array property
-                          array: [ 1, 2, 3, 4 ],
-                          // An object property
-                          object: {
-                              lang: "en-Us"
-                          },
-                          getData: function() {
-                              // get the current value of `data`
-                              return data;
-                          },
-                          setHeight: function( value ) {
-                              // set the value of `data` and return it
-                              console.log('Got here');
-                              var elem = document.getElementById('headerImage');
-                              return (
-                                   elem.setAttribute( 'style', 'height:'+value+'px' )
-                                     );
-                              
-                          },
-                          
-                          helloWorld: function( value ) {
-                              // set the value of `data` and return it
-                              return ( console.log('Hello World.'+window.innerHeight) );
-                          }
-                      };
+            var data = "secret";
+            return {
+                bool: true,
+                header: document.getElementById('headerImage'),
+
+                scrollTop: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0,
+                                
+                setHeight: function( value, elem ) {
+                    return (elem.setAttribute( 'style', 'height:'+value+'px' ));
+                    
+                },
+
+                helloWorld: function( val ) {
+                    return ( console.log(this.scrollLeft+' '+this.scrollTop) );
+                }
+            };
   })();
-    
-    // Other things might happen here
-    
-    // expose our module to the global object
+
     global.Module = Module;
-    Module.helloWorld();
-    Module.setHeight(window.innerHeight);
-    
+    Module.setHeight(window.innerHeight, Module.header);
+    //Module.helloWorld();
+    //Module.scrollUp(Module.header);
+
+    window.addEventListener('scroll', function() {
+        if(document.body.scrollTop >= 100)
+        {
+            Module.header.classList.add("is-active");
+            console.log('if');
+        } else{
+            Module.header.classList.remove("is-active");
+            console.log('else');
+        }
+        //Module.scrollTop = document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
+        return ( console.log(document.body.scrollTop+' '+Module.scrollTop) );
+    });
+
 })( this );
