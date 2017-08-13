f86fa83a85e84b17032c8079a951f48bb7e72717
Chrisp <ultrachrisp@gmail.com>
2017-08-13 14:32:01 +0200

    Mostly there version, still needs sections.

--- src/js/myScript.js
+++ src/js/myScript.js
@@ -6,6 +6,8 @@
             return {
                 bool: true,
                 header: document.getElementById('headerImage'),
+                title: document.getElementById('homeTitle'),
+                nav: document.getElementById('nav'),
 
                 scrollTop: window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0,
                                 
@@ -21,18 +23,21 @@
   })();
 
     global.Module = Module;
-    Module.setHeight(window.innerHeight, Module.header);
+    if(Module.header)
+        Module.setHeight(window.innerHeight, Module.header);
     //Module.helloWorld();
     //Module.scrollUp(Module.header);
-
+    if(Module.nav)
+        Module.nav.classList.remove("active");
+    
     window.addEventListener('scroll', function() {
-        if(document.body.scrollTop >= 100)
+        if(document.body.scrollTop >= 5)
         {
-            Module.header.classList.add("is-active");
-            console.log('if');
+            Module.nav.classList.add("active");
+            Module.title.classList.remove('active');
         } else{
-            Module.header.classList.remove("is-active");
-            console.log('else');
+            Module.nav.classList.remove("active");
+            Module.title.classList.add('active');
         }
         //Module.scrollTop = document.documentElement.scrollTop || document.body.parentNode.scrollTop || document.body.scrollTop;
         return ( console.log(document.body.scrollTop+' '+Module.scrollTop) );
